dde06e259d5b9b1e68fd0f5f4c035433517789bb
Pablo Carranza <pcarranza@gmail.com>
2018-07-06 20:58:19 +0000

    Add server test

--- server/server_test.go
+++ server/server_test.go
@@ -1,16 +1,29 @@
 package server
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	httpurl "net/url"
+	"os"
+	"strings"
+	"testing"
+
 	"gitlab.com/yakshaving.art/git-pull-mirror/config"
 	"gitlab.com/yakshaving.art/git-pull-mirror/github"
 	"gitlab.com/yakshaving.art/git-pull-mirror/url"
-	"testing"
+	git "gopkg.in/src-d/go-git.v4"
 )
 
 func TestBuildingAServerAndConfigureWithEmptyConfigWorks(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "server_test")
+	must(t, "could not create a temporary dir", err)
+
+	defer os.RemoveAll(tmpDir)
+
 	s := New(WebHooksServerOptions{
-		GitTimeoutSeconds:        60,
-		RepositoriesPath:         "/tmp",
+		GitTimeoutSeconds:        10,
+		RepositoriesPath:         tmpDir,
 		SkipWebhooksRegistration: true,
 		GitHubClientOpts: github.ClientOpts{
 			CallbackURL: "https://example.com/",
@@ -19,10 +32,19 @@ func TestBuildingAServerAndConfigureWithEmptyConfigWorks(t *testing.T) {
 		},
 	})
 	originURL, err := url.Parse("https://github.com/yakshaving-art/git-pull-mirror.git")
-	must(t, err)
+	must(t, "could not parse origin url", err)
 
-	targetURL, err := url.Parse("https://gitlab.com/yakshaving.art/git-pull-mirror.git")
-	must(t, err)
+	targetURL := url.GitURL{
+		Domain:    "gitlab.com",
+		Name:      "git-pull-mirror",
+		Owner:     "yakshaving.art",
+		URI:       "file://" + tmpDir + "/target/gitlab.com/yakshaving.art/git-pull-mirror",
+		Transport: "file",
+	}
+
+	_, err = git.PlainInit(tmpDir+"/gitlab.com/yakshaving.art/git-pull-mirror", true)
+	must(t, "failed to plain init target repo", err)
+	// defer os.RemoveAll(tmpDir)
 
 	if err := s.Configure(config.Config{
 		Repositories: []config.RepositoryConfig{
@@ -38,15 +60,75 @@ func TestBuildingAServerAndConfigureWithEmptyConfigWorks(t *testing.T) {
 	c := make(chan bool)
 	go func() {
 		c <- true
-		s.Run("localhost:9092")
+		s.Run(":9092")
 	}()
 	<-c
+	defer s.Shutdown()
 
-	s.Shutdown()
+	tt := []struct {
+		name string
+		test func(*testing.T)
+	}{
+		{
+			"Correct webhook invocation",
+			func(t *testing.T) {
+				res, err := runWebhook(s.callbackPath, "yakshaving-art/git-pull-mirror")
+				must(t, "failed to execute webhooks 1", err)
+
+				if res.Status != "202 Accepted" {
+					t.Fatalf("Unexpected status code %s", res.Status)
+				}
+			},
+		},
+		{
+			"Invalid repo name",
+			func(t *testing.T) {
+				res, err := runWebhook(s.callbackPath, "yakshaving-art")
+				must(t, "failed to execute webhooks 2", err)
+
+				if res.Status != "404 Not Found" {
+					t.Fatalf("Unexpected status code %s", res.Status)
+				}
+			},
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.test(t)
+		})
+	}
+
 }
 
-func must(t *testing.T, err error) {
+func must(t *testing.T, desc string, err error) {
 	if err != nil {
-		t.Fatalf("Got error %s", err)
+		t.Fatalf("%s, got error %s", desc, err)
 	}
+}
+
+func runWebhook(path, fullname string) (*http.Response, error) {
+	b, err := json.Marshal(github.HookPayload{
+		Repository: github.Repository{
+			FullName: fullname,
+		},
+		Hook: github.Hook{
+			Events: []string{"push"},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	form := httpurl.Values{}
+	form.Add("payload", string(b))
+
+	serverURL := "http://localhost:9092" + path
+
+	req, err := http.NewRequest("POST", serverURL, strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		return nil, err
+	}
+
+	return http.DefaultClient.Do(req)
 }
