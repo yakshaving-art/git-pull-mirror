19f104f6ff4cba71986a2b895d1acaa4c9d6eb59
Pablo Carranza <pcarranza@gmail.com>
2018-08-21 20:03:54 +0200

    Add version flag

---
+++ version/version.go
@@ -0,0 +1,13 @@
+package version
+
+// Name is the application name
+var Name = "git-pull-mirror"
+
+// Version is the application Version
+var Version string
+
+// Date is the built date and time
+var Date string
+
+// Commit is the commit in which the package is based
+var Commit string
